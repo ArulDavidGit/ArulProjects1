6e976afdb2116829bbe92c43a214e1b9d0980b2c
Arul David <arul01121985@gmail.com>
2016-03-12 14:42:24 -0800

    Added the invoking metod to main method

---
+++ GitDemo1/src/GitTest.java
@@ -0,0 +1,17 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sayHello();
+
+	}
+	
+	public static void sayHello(){
+		
+		System.out.println(" This is I am saying Hello");
+		
+	}
+
+}
